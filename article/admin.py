6ce643fa42c098ed714fd6ed55980baf8126089b
xx_rubix_xx <miroshnichenkoartem0307@gmail.com>
2025-11-16 03:02:15 +0200

    added article models

--- article/admin.py
+++ article/admin.py
@@ -1,1 +1,37 @@
 from django.contrib import admin
+from django.db.models import Value, Case, When, TextField
+from django.db.models.functions import Concat, Substr, Length
+
+from article.models import Article
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'short_text_content', 'topic__name')
+    list_filter = ('topic',)
+
+    list_select_related = ('topic',)
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+
+        queryset = (
+            queryset
+            .only('id', 'title', 'topic_id',)
+            .annotate(text_len=Length('text'))
+            .annotate(
+                short_text_content=Case(
+                    When(
+                        text_len__gt=150,
+                        then=Concat(Substr('text', 1, 150), Value('...'))
+                    ),
+                    default='text',
+                    output_field=TextField(),
+                )
+            )
+        )
+        return queryset
+
+    @admin.display(description='Short text content')
+    def short_text_content(self, obj):
+        return obj.short_text_content
--- article/models.py
+++ article/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Name')
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=512, unique=True, verbose_name='Title')
+    text = models.TextField(verbose_name='Text')
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, related_name='articles', null=True)
+
+    def __str__(self):
+        return self.title
